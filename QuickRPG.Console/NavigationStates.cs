c8985e6a852d7c71e091bddbdbae140dcd983298
Pascal Laurin <pascal.laurin@outlook.com>
2025-10-14 08:00:04 -0400

    .NET 9

---
+++ QuickRPG.Console/NavigationStates.cs
@@ -0,0 +1,23 @@
+﻿namespace QuickRPG.Console;
+
+internal enum NavigationStates
+{
+    NoGameLoaded,
+    GameLoaded,
+    GalleryMain,
+    EnemiesGallery,
+    EnemyDetails,
+}
+
+internal enum NavigationTriggers
+{
+    LoadGame,
+    ChangeGame,
+    OpenGallery,
+    CloseGallery,
+    OpenEnemiesGallery,
+    CloseEnemiesGallery,
+    OpenEnemyDetails,
+    CloseEnemyDetails,
+    NextEnemyDetails,
+}
